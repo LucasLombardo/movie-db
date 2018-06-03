2665e2426465574f44390d5ba186cd460ba6e074
Lucas Lombardo <Lucasl@bu.edu>
2018-06-02 23:17:36 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie';
+
+class App extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=6f189bc3887975f19dd52bd3a24b36d7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Route path="/test" component={Test} />
+          {this.state.movies.map(movie => (<Movie key={movie.id} movie={movie} />))}
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
+
+
+const Test = () => (
+  <h1>Test</h1>
+);
